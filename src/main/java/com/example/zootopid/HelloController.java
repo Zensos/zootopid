85af71de737577e1db275859ba87ce2b86e97f18
Zensos <55834323+Zensos@users.noreply.github.com>
2024-02-25 15:55:33 +0700

    test: commit

--- src/main/java/com/example/zootopid/HelloController.java
+++ src/main/java/com/example/zootopid/HelloController.java
@@ -31,6 +31,7 @@ public class HelloController {
         animals.put("Dog", Dog.class);
         animals.put("Panda", Panda.class);
         animals.put("Crocodile", Crocodile.class);
+
     }
 
     @FXML
